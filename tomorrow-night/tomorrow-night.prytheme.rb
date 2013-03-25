93e0600151792991205833f0819ad7898129d344
Kyrylo Silin <kyrylosilin@gmail.com>
2013-03-25 22:47:45 +0200

    TomorrowNight: convert to the new syntax

--- tomorrow-night/tomorrow-night.prytheme.rb
+++ tomorrow-night/tomorrow-night.prytheme.rb
@@ -1,43 +1,48 @@
----
-meta:
-  theme-name  : tomorrow-night
-  version     : 8
-  color-depth : 256
-  description : You should have Tomorrow-Night colors as your terminal colors
-  author      : Jeff Sandberg <paradox460@gmail.com>,
-                Chris Kempson
+t = PryTheme.create :name => 'tomorrow-night' do
+  author :name => 'Jeff Sandberg', :email => 'paradox460@gmail.com'
+  description 'You should have Tomorrow-Night colors as your terminal colors'
 
-theme:
-  class               : yellow
-  class_variable      : red
-  comment             : gray
-  constant            : red
-  error               : black (u) on maroon
-  float               : olive
-  global_variable     : red
-  inline_delimiter    : green
-  instance_variable   : red
-  integer             : olive
-  keyword             : magenta
-  method              : navy_blue
-  predefined_constant : cyan
-  regexp:
-    self              : red
-    char              : green
-    content           : red
-    delimiter         : red
-    modifier          : red
-    escape            : green
-  shell:
-    self              : toad_in_love
-    char              : green
-    content           : green
-    delimiter         : green
-    escape            : green
-  string:
-    self              : green
-    char              : green
-    content           : green
-    delimiter         : green
-    escape            : green
-  symbol              : green (b)
+  define_theme do
+    class_ 'yellow'
+    class_variable 'red'
+    comment 'gray'
+    constant 'red'
+    error 'black', 'maroon', [:underline]
+    float 'olive'
+    global_variable 'red'
+    inline_delimiter 'green'
+    instance_variable 'red'
+    integer 'olive'
+    keyword 'magenta'
+    method 'navy_blue'
+    predefined_constant 'cyan'
+    symbol 'green', [:bold]
+
+    regexp do
+      self_ 'red'
+      char 'green'
+      content 'red'
+      delimiter 'red'
+      modifier 'red'
+      escape 'green'
+    end
+
+    shell do
+      self_ 'toad_in_love'
+      char 'green'
+      content 'green'
+      delimiter 'green'
+      escape 'green'
+    end
+
+    string do
+      self_ 'green'
+      char 'green'
+      content 'green'
+      delimiter 'green'
+      escape 'green'
+    end
+  end
+end
+
+PryTheme::ThemeList.add_theme(t)
